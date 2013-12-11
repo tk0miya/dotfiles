6500aa792790da4e66695db5f56624accf6bd7d8
tk0miya <i.tkomiya@gmail.com>
2013-12-11 12:56:25 +0900

    Import dotfiles

---
+++ _vimrc
@@ -0,0 +1,14 @@
+if has('vim_starting')
+  set runtimepath+=~/.vim/bundle/neobundle.vim/
+endif
+
+call neobundle#rc(expand('~/.vim/bundle/'))
+
+" Let NeoBundle manage NeoBundle
+NeoBundleFetch 'Shougo/neobundle.vim'
+
+" add plugins
+
+filetype plugin on
+
+NeoBundleCheck
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo ""
+echo "Setup hg extensions ..."
+if [ -d _hgext/hgbb ]
+then
+    (cd _hgext/hgbb && hg update)
+else
+    (cd _hgext && hg clone https://bitbucket.org/birkenfeld/hgbb)
+fi
+
+echo ""
+echo "Setup vim extensions ..."
+if [ -d _vim/bundle/neobundle.vim ]
+then
+    (cd _vim/bundle/neobundle.vim  && git pull)
+else
+    git clone https://github.com/Shougo/neobundle.vim _vim/bundle/neobundle.vim
+fi
+
+echo ""
+echo "Creating dotfile symlinks ..."
+for dotfile in _?*
+do
+    dotname=`echo $dotfile | sed -e "s/^_/./"`
+    if [ $dotfile != '..' ] && [ $dotfile != '.hg' ]; then
+        if [ -L $HOME/$dotname ]; then
+            cat /dev/null  # noop
+        elif [ -e $HOME/$dotname ]; then
+            echo "Ignore $HOME/$dotname (already exists)"
+        else
+            echo "Creating $HOME/$dotname ..."
+            ln -s "$PWD/$dotfile" $HOME/$dotname
+        fi
+    fi
+done
