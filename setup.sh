8d4f7de51daaf373e990c97d43581605e3d5bd78
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-05-06 20:27:44 +0900

    pyenv: Install 3.4.4

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ `hostname` = "deneb" ]; then
-    REGULAR_PYTHON_VERSIONS="2.7.11 2.6.9 3.5.1 3.4.3 3.3.6"
+    REGULAR_PYTHON_VERSIONS="2.7.11 2.6.9 3.5.1 3.4.4 3.3.6"
 elif [ `hostname` = "capella" ]; then
     REGULAR_PYTHON_VERSIONS="2.7.11 3.5.1"
 else
