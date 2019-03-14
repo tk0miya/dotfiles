7a76b2d94dccd5bd768f818f893f34aeb39acd27
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-03-15 01:55:34 +0900

    pyenv: Install 2.7.16

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ `hostname` = "deneb.local" ]; then
-    REGULAR_PYTHON_VERSIONS="3.7.2 3.6.8 3.5.6 3.4.9 3.3.7 2.7.15"
+    REGULAR_PYTHON_VERSIONS="3.7.2 3.6.8 3.5.6 3.4.9 3.3.7 2.7.16"
 elif [ `hostname` = "capella" ]; then
-    REGULAR_PYTHON_VERSIONS="3.7.2 2.7.15"
+    REGULAR_PYTHON_VERSIONS="3.7.2 2.7.16"
 else
     REGULAR_PYTHON_VERSIONS="3.7.2"
 fi
