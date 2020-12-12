e3d8d75df2126b528215e6be5450a61dc9433cfd
Takeshi KOMIYA <i.tkomiya@gmail.com>
2020-12-12 22:48:29 +0900

    pyenv: INstall 3.9.1

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ `hostname` = "tarf.local" -o `hostname` = "Alrescha.local" ]; then
-    REGULAR_PYTHON_VERSIONS="3.9.0 3.8.6 3.7.9 3.6.12 3.5.10 3.10-dev"
+    REGULAR_PYTHON_VERSIONS="3.9.1 3.8.6 3.7.9 3.6.12 3.5.10 3.10-dev"
 else
-    REGULAR_PYTHON_VERSIONS="3.9.0"
+    REGULAR_PYTHON_VERSIONS="3.9.1"
 fi
 
 mkdir -p $HOME/bin
