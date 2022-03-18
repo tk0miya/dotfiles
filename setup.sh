c80802cf60d0031daa22a68d67371af825e6b52e
Takeshi KOMIYA <i.tkomiya@gmail.com>
2022-03-19 01:29:22 +0900

    pyenv: Install 3.10.3, 3.9.11, 3.8.13, and 3.7.13

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ `hostname` = "tarf.local" -o `hostname` = "Alrescha.local" ]; then
-    REGULAR_PYTHON_VERSIONS="3.10.0 3.9.9 3.8.12 3.7.12 3.6.15 3.11-dev"
+    REGULAR_PYTHON_VERSIONS="3.10.3 3.9.11 3.8.13 3.7.13 3.6.15 3.11-dev"
 else
-    REGULAR_PYTHON_VERSIONS="3.10.0"
+    REGULAR_PYTHON_VERSIONS="3.10.3"
 fi
 
 mkdir -p $HOME/bin
